4a5a36325637a7520ce4d61d529a5513656d2b24
Per Zetterlund <per@pz.se>
2021-12-02 20:13:08 +0100

    Day 4

--- 04/04.py
+++ 04/04.py
@@ -36,30 +36,64 @@ def f01():
 
 
 def f02():
-    verify = [('byr', lambda a : 1920 <= int(a) <= 2002),
-              ('iyr', lambda a : 2010 <= int(a) <= 2020),
-              ('eyr', lambda a : 2020 <= int(a) <= 2030),
-              ('hgt', lambda a : 59 <= int(a[:-2]) <= 76 if a[-2:] == 'in' else 150 <= int(a[:-2]) <= 193),
-              ('hcl', lambda a : a[0] == '#' and 
-              'ecl',
-              'pid']
-
+    keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
     with open('input.txt') as file:
         passports = file.read().split('\n\n')
 
         count = 0
 
         for p in passports:
+            akeys = [a.split(':')[0] for a in p.split()]
+
+            if sum(akeys.count(k) > 0 for k in keys) != 7:
+                continue
+
             elements = [a.split(':') for a in p.split()]
 
+            ok = True
             for e in elements:
+                a = e[1]
                 if e[0] == 'byr':
+                    if not 1920 <= int(a) <= 2002:
+                        ok = False
+                if e[0] == 'iyr':
+                    if not 2010 <= int(a) <= 2020:
+                        ok = False
+                if e[0] == 'eyr':
+                    if not 2020 <= int(a) <= 2030:
+                        ok = False
+                if e[0] == 'hgt':
+                    #print(a[-2:], a[:-2])
+                    if a[-2:] == 'in':
+                        if not 59 <= int(a[:-2]) <= 76:
+                            ok = False
+                    elif a[-2:] == 'cm':
+                        if not 150 <= int(a[:-2]) <= 193:
+                            ok = False
+                    else:
+                        ok = False
+                if e[0] == 'hcl':
+                    if len(a) != 7:
+                        ok = False
+                    if a[0] != '#':
+                        ok = False
+                    if not all(e in "01234567890abcdef" for e in a[1:]):
+                        ok = False
+                if e[0] == 'ecl':
+                    if a not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+                        ok = False
+                if e[0] == 'pid':
+                    if len(a) != 9 or not a.isnumeric():
+                        ok = False
 
+                if not ok:
+                    break
+            if not ok:
+                continue
 
-            if sum(akeys.count(k) > 0 for k in keys) == 7:
-                count += 1
+            count += 1
 
-    print(count)
+        print(count)
 
 
 def main():
