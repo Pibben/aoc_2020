cb5de707db4fe4590f1ea93ffd395c3eb5877fcd
Per Zetterlund <per@pz.se>
2021-12-17 14:52:33 +0100

    Day 23 Part 2

--- 23/23.py
+++ 23/23.py
@@ -29,35 +29,147 @@ def msplit(s, ds=None):
     return res
 
 
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def link(self, nxt):
+        self.next = nxt
+
+    def __str__(self):
+        return ' '.join(str(a) for a in self.to_list())
+
+    def cut_right(self, num):
+        new_head = self.next
+        curr = self
+        for i in range(num):
+            curr = curr.next
+        self.next = curr.next
+        curr.next = new_head
+
+        return new_head
+
+    def insert_right(self, new):
+        old_next = self.next
+        self.next = new
+        curr = new
+        while curr.next != new:
+            curr = curr.next
+        assert(curr.next == new)
+        curr.next = old_next
+
+    def __len__(self):
+        count = 1
+        curr = self.next
+        while curr != self:
+            count += 1
+            curr = curr.next
+        return count
+
+    def to_list(self):
+        curr = self
+        ret = []
+        while True:
+            ret.append(curr.val)
+            curr = curr.next
+            if curr == self:
+                break
+        return ret
+
+    def contains(self, key):
+        curr = self
+        while True:
+            if curr.val == key:
+                return True
+            curr = curr.next
+            if curr == self:
+                break
+        return False
+
+
+def create_from_list(plist):
+    map = {}
+    prev = None
+    head = None
+    n = None
+    for e in plist:
+        n = Node(e)
+        map[e] = n
+        if prev is not None:
+            prev.link(n)
+        prev = n
+        if head is None:
+            head = n
+
+    if n is not None:
+        n.link(head)
+
+    return head, map
+
+
 def f01():
     with open('input') as file:
         start = toInt(list(file.readline()[:-1]))
         max_num = max(start)
         min_num = min(start)
+        length = len(start)
+
+        l, m = create_from_list(start)
+
+        current_label = start[0]
 
         for _ in range(100):
-            current = start[0]
-            pick = start[1:4]
-            rest = start[4:]
-            dst_label = current - 1
-            while dst_label not in rest:
+            pick = m[current_label].cut_right(3)
+            dst_label = current_label - 1
+            if dst_label < min_num:
+                dst_label = max_num
+            while pick.contains(dst_label):
                 dst_label -= 1
                 if dst_label < min_num:
                     dst_label = max_num
-            dst_idx = rest.index(dst_label) + 1
 
-            new = rest[:dst_idx] + pick + rest[dst_idx:] + [current]
-            start = new
+            m[dst_label].insert_right(pick)
+            assert(len(m[1]) == length)
+            current_label = m[current_label].next.val
 
-        idx = start.index(1)
-        result = start[idx:] + start[:idx]
-        result = ''.join(str(i) for i in result[1:])
+        result = ''.join(str(i) for i in m[1].to_list()[1:])
         print(result)
         assert(result == '62934785')
 
 
 def f02():
-    pass
+    with open('input') as file:
+        start = toInt(list(file.readline()[:-1]))
+        max_num = max(start)
+        min_num = min(start)
+
+        start.extend([i for i in range(max_num + 1, 1000001)])
+
+        length = len(start)
+        assert(length == 1000000)
+        max_num = 1000000
+
+        l, m = create_from_list(start)
+
+        current_label = start[0]
+
+        for _ in range(10*1000*1000):
+            pick = m[current_label].cut_right(3)
+            dst_label = current_label - 1
+            if dst_label < min_num:
+                dst_label = max_num
+            while pick.contains(dst_label):
+                dst_label -= 1
+                if dst_label < min_num:
+                    dst_label = max_num
+
+            m[dst_label].insert_right(pick)
+            current_label = m[current_label].next.val
+
+        result = m[1].next.val * m[1].next.next.val
+        print(result)
+        assert(result == 693659135400)
 
 
 def main():
